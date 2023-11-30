c14b01ee14866e56ba6a4216fe23e1a82a0db81d
mdleye <mdleye@users.noreply.github.com>
2023-11-30 18:39:13 +0000

    Updated template to latest version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     darkMode: 'class',
     content: [
         "./build/html/*.html",
-        "./build/html/activity/activity-8848645176.html",
+        "./build/html/**/activity-8837881433.html",
+        "./build/html/month/*.html",
         "./node_modules/flowbite/**/*.js"
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'strava-orange': '#F26722',
+            },
+        },
     },
     plugins: [
         require('flowbite/plugin')
     ]
 }
-
