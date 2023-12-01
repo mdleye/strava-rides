946d90f64e3ee5ab7c59e343a7cbd42986ea5060
mdleye <mdleye@users.noreply.github.com>
2023-12-01 18:38:50 +0000

    Updated template to latest version

--- src/Domain/Strava/MonthlyStatistics.php
+++ src/Domain/Strava/MonthlyStatistics.php
@@ -5,55 +5,56 @@ namespace App\Domain\Strava;
 use App\Domain\Strava\Activity\Activity;
 use App\Domain\Strava\Activity\ActivityCollection;
 use App\Domain\Strava\Activity\ActivityType;
+use App\Domain\Strava\Calendar\Month;
+use App\Domain\Strava\Calendar\MonthCollection;
 use App\Domain\Strava\Challenge\Challenge;
 use App\Domain\Strava\Challenge\ChallengeCollection;
-use App\Infrastructure\ValueObject\Time\SerializableDateTime;
 use Carbon\CarbonInterval;
 
 final readonly class MonthlyStatistics
 {
+    /** @var array<mixed> */
+    private array $statistics;
+
     private function __construct(
         private ActivityCollection $activities,
         private ChallengeCollection $challenges,
-        private SerializableDateTime $now,
+        private MonthCollection $months,
     ) {
+        $this->statistics = $this->buildStatistics();
     }
 
     public static function fromActivitiesAndChallenges(
         ActivityCollection $activities,
         ChallengeCollection $challenges,
-        SerializableDateTime $now): self
+        MonthCollection $months): self
     {
-        return new self($activities, $challenges, $now);
+        return new self(
+            activities: $activities,
+            challenges: $challenges,
+            months: $months
+        );
     }
 
     /**
      * @return array<mixed>
      */
-    public function getRows(): array
+    private function buildStatistics(): array
     {
         $statistics = [];
-        $startDate = $this->activities->getFirstActivityStartDate();
-
-        $interval = new \DateInterval('P1M');
-        $period = new \DatePeriod(
-            $startDate->modify('first day of this month'),
-            $interval,
-            $this->now->modify('last day of this month')
-        );
-
-        foreach ($period as $date) {
-            $month = $date->format('Ym');
-            $statistics[$month] = [
-                'id' => $date->format('Y-m'),
-                'month' => $date->format('F Y'),
+        /** @var \App\Domain\Strava\Calendar\Month $month */
+        foreach ($this->months as $month) {
+            $statistics[$month->getId()] = [
+                'id' => $month->getId(),
+                'month' => $month->getLabel(),
                 'numberOfRides' => 0,
                 'totalDistance' => 0,
                 'totalElevation' => 0,
-                'movingTime' => 0,
+                'totalCalories' => 0,
+                'movingTimeInSeconds' => 0,
                 'challengesCompleted' => count(array_filter(
                     $this->challenges->toArray(),
-                    fn (Challenge $challenge) => $challenge->getCreatedOn()->format('Ym') == $date->format('Ym')
+                    fn (Challenge $challenge) => $challenge->getCreatedOn()->format(Month::MONTH_ID_FORMAT) == $month->getId()
                 )),
             ];
         }
@@ -62,21 +63,38 @@ final readonly class MonthlyStatistics
 
         /** @var Activity $activity */
         foreach ($this->activities as $activity) {
-            $month = $activity->getStartDate()->format('Ym');
+            $month = $activity->getStartDate()->format(Month::MONTH_ID_FORMAT);
 
             ++$statistics[$month]['numberOfRides'];
             $statistics[$month]['totalDistance'] += $activity->getDistance();
             $statistics[$month]['totalElevation'] += $activity->getElevation();
-            $statistics[$month]['movingTime'] += $activity->getMovingTimeInSeconds();
+            $statistics[$month]['movingTimeInSeconds'] += $activity->getMovingTimeInSeconds();
+            $statistics[$month]['totalCalories'] += $activity->getCalories();
         }
 
         $statistics = array_filter($statistics, fn (array $statistic) => $statistic['numberOfRides'] > 0);
 
         foreach ($statistics as &$statistic) {
-            $statistic['movingTime'] = CarbonInterval::seconds($statistic['movingTime'])->cascade()->forHumans(['short' => true, 'minimumUnit' => 'minute']);
+            $statistic['movingTime'] = CarbonInterval::seconds($statistic['movingTimeInSeconds'])->cascade()->forHumans(['short' => true, 'minimumUnit' => 'minute']);
         }
 
         return $statistics;
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function getStatistics(): array
+    {
+        return $this->statistics;
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function getStatisticsForMonth(Month $month): array
+    {
+        return $this->statistics[$month->getId()] ?? [];
     }
 
     /**
@@ -124,6 +142,7 @@ final readonly class MonthlyStatistics
             'numberOfRides' => count($activities),
             'totalDistance' => array_sum(array_map(fn (Activity $activity) => $activity->getDistance(), $activities)),
             'totalElevation' => array_sum(array_map(fn (Activity $activity) => $activity->getElevation(), $activities)),
+            'totalCalories' => array_sum(array_map(fn (Activity $activity) => $activity->getCalories(), $activities)),
             'movingTime' => CarbonInterval::seconds(array_sum(array_map(fn (Activity $activity) => $activity->getMovingTimeInSeconds(), $activities)))->cascade()->forHumans(['short' => true, 'minimumUnit' => 'minute']),
         ];
     }
