86249f0f07f80e0348b05b716abd6c0b0480277d
mdleye <mdleye@users.noreply.github.com>
2023-12-02 18:35:23 +0000

    Updated template to latest version

--- src/Domain/Strava/Activity/ActivityCollection.php
+++ src/Domain/Strava/Activity/ActivityCollection.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Domain\Strava\Activity;
 
+use App\Domain\Strava\Calendar\Month;
 use App\Infrastructure\ValueObject\Collection;
 use App\Infrastructure\ValueObject\Time\SerializableDateTime;
 
@@ -35,6 +36,30 @@ final class ActivityCollection extends Collection
         return ActivityCollection::fromArray(array_filter(
             $this->toArray(),
             fn (Activity $activity) => $activity->getStartDate()->format('Ymd') === $date->format('Ymd')
+        ));
+    }
+
+    public function filterOnMonth(Month $month): ActivityCollection
+    {
+        return ActivityCollection::fromArray(array_filter(
+            $this->toArray(),
+            fn (Activity $activity) => $activity->getStartDate()->format(Month::MONTH_ID_FORMAT) === $month->getId()
+        ));
+    }
+
+    public function filterOnDateRange(SerializableDateTime $fromDate, SerializableDateTime $toDate): ActivityCollection
+    {
+        return ActivityCollection::fromArray(array_filter(
+            $this->toArray(),
+            fn (Activity $activity) => $activity->getStartDate()->isAfterOrOn($fromDate) && $activity->getStartDate()->isBeforeOrOn($toDate)
+        ));
+    }
+
+    public function filterOnActivityType(ActivityType $activityType): ActivityCollection
+    {
+        return ActivityCollection::fromArray(array_filter(
+            $this->toArray(),
+            fn (Activity $activity) => $activityType === $activity->getType()
         ));
     }
 }
